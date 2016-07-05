5d2ee972a606a421e7dbd71857f3988e4233a863
Douglas Queiroz <douglasq16@gmail.com>
2016-07-05 15:04:33 -0300

    Update pportugues string file

--- translate.rb
+++ translate.rb
@@ -1,29 +1,49 @@
-# encoding: UTF-8
-require 'spreadsheet' 
-book = Spreadsheet.open('myexcel.xls')
-excelLanguage = book.worksheet('Original')
-languageColumn = 1
+
+languageColumn = 0
+
+traducoes = []
+
+File.open("Planilha", 'r') do |file|
+	file.each do |line|
+		traducoes << line.split("|")
+	end
+end
 
 path = 'ingles'
+
+new_traducoes = []
 lines = IO.readlines(path).map
 
-new_strings = []
+i = 0
 lines.each do |line|
+	i += 1
 
 	text = /<<.+[0-9]>>/.match(line)
 	if text
 		row = text.to_s.gsub(/<<|>>/, "").to_i
+		
+		newString = traducoes[row-1][languageColumn]
+		if row == 9
+			puts newString	
+		end
 
-		puts row
-		newString = excelLanguage.cell(543,languageColumn)
-		# new_strings << line.gsub(text.to_s, newString)
+		new_traducoes << line.gsub(text.to_s, newString)
+
+		if i == 83
+			puts line
+			puts "passou 1"
+		end
 	else
-		new_strings << line
+		new_traducoes << line
+		if i == 83
+			puts line
+			puts "passou 2"
+		end
 	end
 end
 
 File.open("new_strings", 'w') do |file|
-	new_strings.each do |line|
+	new_traducoes.each do |line|
 		file.puts line
 	end
 end
